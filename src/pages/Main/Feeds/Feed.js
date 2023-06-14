5313bf7ea409f22f7dc603948a76c5e7186a8004
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-06-15 02:39:40 +0900

    MODIFY: 불필요한 코드 삭제, 이미지 alt 내용 안 적은 것 추가

--- src/pages/Main/Feeds/Feed.js
+++ src/pages/Main/Feeds/Feed.js
@@ -50,82 +50,78 @@ const Feed = ({
   };
 
   return (
-    <>
-      <div className="feed-wrap" key={id} onDoubleClick={toggleLike}>
-        <div className="profile-wrap">
-          <FeedHeader
-            key={id}
-            userFeedProfile={userFeedProfile}
-            account={account}
-          />
-          <FeedContent feedImgsrc={feedImgsrc} />
+    <div className="feed-wrap" key={id} onDoubleClick={toggleLike}>
+      <div className="profile-wrap">
+        <FeedHeader
+          key={id}
+          userFeedProfile={userFeedProfile}
+          account={account}
+        />
+        <FeedContent feedImgsrc={feedImgsrc} />
 
-          <div className="feed-under-wrap">
-            <div className="under-button-wrap">
-              <div className="comment-div">
-                <div className="people-like-div">
+        <div className="feed-under-wrap">
+          <div className="under-button-wrap">
+            <div className="comment-div">
+              <div className="people-like-div">
+                <i
+                  className={`fa-heart ${like ? 'fas is-liked' : 'far'}`}
+                  onClick={toggleLike}
+                />
+
+                <span className="people-like-span">
+                  좋아요 {like ? likeHit + 1 : likeHit}개
+                </span>
+              </div>
+
+              <div className="my-comment-wrap">
+                <span className="account">{account}</span>
+                <span>{feedText}</span>
+              </div>
+
+              <div className="pre-comment-wrap">
+                <span className="pre-account-name"> {replyAccount}</span>
+                <span className="pre-reply">{mockReply}</span>
+
+                <div className="pre-icon-wrap">
                   <i
-                    className={`fa-heart ${like ? 'fas is-liked' : 'far'}`}
-                    onClick={toggleLike}
+                    className={`fa-heart ${replyLike ? 'fas is-liked' : 'far'}`}
+                    onClick={replyToggleLike}
                   />
-
-                  <span className="people-like-span">
-                    좋아요 {like ? likeHit + 1 : likeHit}개
-                  </span>
                 </div>
-
-                <div className="my-comment-wrap">
-                  <span className="account">{account}</span>
-                  <span>{feedText}</span>
-                </div>
-
-                <div className="pre-comment-wrap">
-                  <span className="pre-account-name"> {replyAccount}</span>
-                  <span className="pre-reply">{mockReply}</span>
-
-                  <div className="pre-icon-wrap">
-                    <i
-                      className={`fa-heart ${
-                        replyLike ? 'fas is-liked' : 'far'
-                      }`}
-                      onClick={replyToggleLike}
-                    />
-                  </div>
-                </div>
-                {comment.map(({ id, account, reply }) => {
-                  return (
-                    <Comment
-                      id={id}
-                      key={id}
-                      account={account}
-                      reply={reply}
-                      removeReply={removeReply}
-                    />
-                  );
-                })}
               </div>
-
-              <div className="comment-register-wrap">
-                <form className="comment-form" onSubmit={submitComment}>
-                  <input
-                    className="addcomment-input"
-                    placeholder="댓글 달기..."
-                    onChange={getReply}
-                    value={reply}
+              {comment.map(({ id, account, reply }) => {
+                return (
+                  <Comment
+                    id={id}
+                    key={id}
+                    account={account}
+                    reply={reply}
+                    removeReply={removeReply}
                   />
+                );
+              })}
+            </div>
 
-                  <button
-                    className={reply.length >= 1 ? 'submit-on' : 'submit-off'}
-                  >
-                    게시
-                  </button>
-                </form>
-              </div>
+            <div className="comment-register-wrap">
+              <form className="comment-form" onSubmit={submitComment}>
+                <input
+                  className="addcomment-input"
+                  placeholder="댓글 달기..."
+                  onChange={getReply}
+                  value={reply}
+                />
+
+                <button
+                  className={reply.length >= 1 ? 'submit-on' : 'submit-off'}
+                >
+                  게시
+                </button>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Main/Feeds/FeedContent/FeedContent.js
+++ src/pages/Main/Feeds/FeedContent/FeedContent.js
@@ -4,7 +4,7 @@ import './FeedContent.scss';
 const FeedContent = ({ feedImgsrc }) => {
   return (
     <div className="feed-image-wrap">
-      <img src={feedImgsrc} className="feed-image"></img>
+      <img src={feedImgsrc} className="feed-image" alt="피드 이미지"></img>
     </div>
   );
 };
